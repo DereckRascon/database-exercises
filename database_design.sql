9cacbc0f176fc4ed606032342b2dba2465079662
Dereck Rascon <dereckrascon76@gmail.com>
2023-02-01 09:24:52 -0700

    completed the database design project

--- database_design.sql
+++ database_design.sql
@@ -1,4 +1,6 @@
+DROP DATABASE database_design_db;
 CREATE DATABASE database_design_db;
+
 
 USE database_design_db;
 
@@ -32,6 +34,36 @@ CREATE TABLE ads_category(
                              FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
+INSERT INTO user (username, user_email, user_password)
+    VALUES ('kiki', 'kiki@hotmail.com','kikirules'),
+           ('don', 'donjon@hotmail.com','donrocks'),
+           ('roe', 'roe@hotmail.com','roe89');
+
+SELECT * FROM user;
+
+INSERT INTO categories (category_name)
+    VALUES('help wanted'),
+          ('giveaway'),
+          ('furniture');
+
+SELECT * FROM categories;
+
+DESCRIBE ads;
+
+INSERT INTO ads(ad_title, ad_description, user_id)
+    VALUES('gardner', 'pulling weeds', 1),
+          ('ipad', 'used', 2),
+          ('sofa', 'almost new', 3);
+
+SELECT * FROM ads;
+
+INSERT INTO ads_category(ads_id, category_id) VALUES
+        (1, 1), (2, 2), (3, 3);
+
+SELECT * FROM ads_category;
+
+
+
 
 #For a given ad, what is the email address of the user that created it?
 SELECT user_email
@@ -39,8 +71,18 @@ FROM user
 WHERE id IN(
     SELECT user_id
     FROM ADS
-    WHERE id ='user email' AND ad_title = 'given ad'
+    WHERE id = 'user email' AND ad_title = 'given ad'
 );
+
+SELECT user_email AS 'User E-mail'
+FROM user
+WHERE id IN (
+    SELECT user_id
+    FROM ads
+    WHERE id = 1
+    );
+
+
 #
 # #For a given ad, what category does it belong to?
 # SELECT category_title
@@ -50,7 +92,14 @@ WHERE id IN(
 #     FROM categories_ads
 #     WHERE ads_id = categories_id
 # );
-#
+SELECT category_name as 'Category'
+FROM categories
+WHERE id IN (
+    SELECT category_id
+    FROM ads_category
+    WHERE ads_id = 1
+);
+
 #
 # SELECT category_title
 # FROM categories
@@ -76,6 +125,15 @@ WHERE id IN(
 #         WHERE ads_id ='....'
 #     )
 # );
+
+SELECT ad_title
+FROM ads
+WHERE id IN (
+    SELECT ads_id
+    FROM ads_category
+    WHERE category_id = 3
+    );
+
 #
 #
 # # For a given user, show all the ads they have posted.
@@ -86,3 +144,10 @@ WHERE id IN(
 #     FROM user
 #     WHERE user_email = ''
 # )
+
+SELECT ad_title
+FROM ads
+WHERE user_id IN (
+    SELECT id FROM user
+    WHERE username = 'kiki'
+    );
